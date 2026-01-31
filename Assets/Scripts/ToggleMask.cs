d6869e8ed688ef1fed8e3478fbe66390360386ae
ImpactHayko <haykomamyan2@gmail.com>
2026-01-31 22:20:26 +0400

    commiting Mirror and ToggleMask changes

--- Assets/Scripts/ToggleMask.cs
+++ Assets/Scripts/ToggleMask.cs
@@ -6,12 +6,37 @@ using UnityEngine;
 public class ToggleMask : MonoBehaviour
 {
     public event Action<bool> MaskToggled;
-    
     private bool isMaskOn = false;
+    private bool isAllowedToPutTheMaskOn = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Mirror"))
+        {
+            isAllowedToPutTheMaskOn = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Mirror"))
+        {
+            isAllowedToPutTheMaskOn = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Mirror"))
+        {
+            Debug.Log("Mirror triggers now!");
+        }
+    }
+
 
      void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) 
+        if (Input.GetKeyDown(KeyCode.E) && isAllowedToPutTheMaskOn) 
         {
             toggleMask();
         }
